4e4ad4daa552f2bb699671a28abb223795120875
n4kz <n4kz@n4kz.com>
2017-03-10 00:14:48 +0300

    Updated example

--- example/app.js
+++ example/app.js
@@ -5,19 +5,53 @@ import { TextButton, RaisedTextButton } from 'react-native-material-buttons';
 let styles = {
   scroll: {
     padding: 4,
-    paddingTop: 64,
+    paddingTop: 24,
     backgroundColor: '#E8EAF6',
   },
 
   container: {
     flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'space-around',
     marginBottom: 8,
+    backgroundColor: 'rgba(0,0,0,.05)',
+  },
+
+  column: {
+    justifyContent: 'center',
+    marginBottom: 8,
+    backgroundColor: 'rgba(0,0,0,.05)',
   },
 
   row: {
     marginBottom: 8,
+  },
+
+  card: {
+    borderRadius: 2,
+    padding: 8,
+    margin: 4,
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    minHeight: 76,
+    justifyContent: 'space-between',
+    shadowOpacity: 0.54,
+    shadowRadius: 1,
+    shadowOffset: { width: 0, height: 1 },
+    elevation: 1,
+  },
+
+  button: {
+    margin: 4,
+  },
+
+  display: {
+    paddingHorizontal: 8,
+    fontSize: 20,
+    fontWeight: '500',
+  },
+
+  text: {
+    padding: 8,
+    fontSize: 17,
   },
 };
 
@@ -26,55 +60,39 @@ export default function init() {
     render() {
       return (
         <ScrollView style={styles.scroll}>
-          <View style={styles.container}>
-            <RaisedTextButton title='flat' color='#E040FB' titleColor='white' rippleOpacity={0.54} />
-            <RaisedTextButton title='is' disabledColor='#EA80FC' disabledTitleColor='rgba(255,255,255,.54)' disabled />
-            <RaisedTextButton title='boring' color='#AA00FF' titleColor='white' rippleOpacity={0.54} />
+          <View style={styles.card}>
+            <RaisedTextButton title="default button" />
+            <TextButton title="default flat button" />
           </View>
 
-          <View style={styles.container}>
-            <RaisedTextButton title='use' color='#FFEA00' />
-            <RaisedTextButton title='raised' color='#FFC400' />
+          <View style={styles.card}>
+            <RaisedTextButton title="disabled button" disabled />
+            <TextButton title="disabled flat button" disabled />
           </View>
 
-          <View style={styles.container}>
-            <TextButton title='in' titleColor='#FF5722' />
-            <TextButton title='soviet russia' disabledColor='rgba(255,0,0,.12)' disabledTitleColor='#FF5252' disabled />
+          <View style={styles.card}>
+            <View style={{ flex: 1, paddingVertical: 16 }}>
+              <Text style={styles.display}>raised</Text>
+              <Text style={styles.text}>Buttons with custom color, titleColor, increased rippleDuration and rippleOpacity</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <RaisedTextButton style={styles.button} rippleDuration={600} rippleOpacity={0.54} title='flat'   color='#039BE5' titleColor='white' />
+              <RaisedTextButton style={styles.button} rippleDuration={600} rippleOpacity={0.54} title='is'     color='#0288D1' titleColor='white' />
+              <RaisedTextButton style={styles.button} rippleDuration={600} rippleOpacity={0.54} title='boring' color='#0277BD' titleColor='white' />
+            </View>
           </View>
 
-          <View style={styles.container}>
-            <TextButton title='buttons' titleColor='#00838F' shadeColor='#00B8D4' />
-            <TextButton title='tap' titleColor='#00695C' shadeColor='#00BFA5' />
-            <TextButton title='you' titleColor='#0277BD' shadeColor='#0091EA' />
-          </View>
+          <View style={styles.card}>
+            <View style={{ flex: 1, paddingVertical: 16 }}>
+              <Text style={styles.display}>flat</Text>
+              <Text style={styles.text}>Buttons with custom titleColor</Text>
+            </View>
 
-
-          <View style={styles.row}>
-            <TextButton title='big flat button' titleColor='#D500F9' shadeColor='#D500F9' rippleDuration={750} />
-          </View>
-
-          <View style={styles.row}>
-            <TextButton title='big disabled flat button' disabledTitleColor='#EA80FC' disabled />
-          </View>
-
-          <View style={styles.row}>
-            <TextButton title='big flat button with very long truncated title' titleColor='#AA00FF' shadeColor='#AA00FF' rippleDuration={750} />
-          </View>
-
-          <View style={styles.row}>
-            <RaisedTextButton title='big raised button' color='#76FF03' />
-          </View>
-
-          <View style={styles.row}>
-            <RaisedTextButton title='big disabled raised button' disabledColor='#F4FF81' disabled />
-          </View>
-
-          <View style={styles.row}>
-            <RaisedTextButton title='big raised button with very long truncated title' color='#C6FF00' />
-          </View>
-
-          <View style={styles.row}>
-            <RaisedTextButton title='custom ripple and shade' color='#69F0AE' titleColor='white' shadeColor='#FFFF00' shadeOpacity={0.54} rippleDuration={1200} rippleColor='#D500F9' rippleOpacity={1.0} />
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+              <TextButton titleColor='#00796B' title='decline' />
+              <TextButton titleColor='#00695C' color='rgba(0, 0, 0, .05)' title='accept' />
+            </View>
           </View>
         </ScrollView>
       );
