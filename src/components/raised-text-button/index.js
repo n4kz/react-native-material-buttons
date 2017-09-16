9cb087709db72ff4be297b4b54fb405914d1e86d
S.M.A. Javadi <amin3mej@gmail.com>
2017-09-16 17:14:13 +0430

    add titleStyle

--- src/components/raised-text-button/index.js
+++ src/components/raised-text-button/index.js
@@ -7,23 +7,25 @@ import { styles } from './styles';
 
 export default class RaisedTextButton extends PureComponent {
   static defaultProps = {
+    titleStyle: {},
     titleColor: 'rgb(66, 66, 66)',
     disabledTitleColor: 'rgba(0, 0, 0, .26)',
   };
 
   static propTypes = {
     ...RaisedButton.propTypes,
-
+    titleStyle: PropTypes.object,
     title: PropTypes.string.isRequired,
     titleColor: PropTypes.string,
     disabledTitleColor: PropTypes.string,
   };
 
   render() {
-    let { title, titleColor, disabledTitleColor, ...props } = this.props;
+    let { title, titleColor, disabledTitleColor, titleStyle, ...props } = this.props;
     let { disabled } = this.props;
 
     let titleStyle = {
+      ...titleStyle,
       color: disabled? disabledTitleColor : titleColor,
     };
 
